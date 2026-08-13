42586b8f81eb3a6029d7b0b7555dae8f62abe043
giljihun <accc45@outlook.com>
2026-08-13 09:14:57 +0900

    chore: Xcode 프로젝트 생성 및 빌드 설정 (#2)
    - iOS 26 min target, Swift 6 언어 모드
    - 카메라/마이크/사진 저장 권한 문구 추가
    - Display Name "Please!" 설정 (내부 모듈명은 Please)

---
+++ Please/Please/App/PleaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  PleaseApp.swift
+//  Please
+//
+//  Created by 길지훈 on 8/12/26.
+//
+
+import SwiftUI
+
+@main
+struct PleaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
